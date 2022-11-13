5089b509cb92f3a48cf45448bb9cd190c85a746c
Akos <akosegypro@gmail.com>
2022-11-13 21:39:10 +0900

    feat: v1 almost finished

---
+++ src/components/logos.js
@@ -0,0 +1,31 @@
+export const logos = [{
+    logoUrl: '/assets/typescript.svg',
+    logoAlt: 'Typescript',
+},{
+    logoUrl: '/assets/csharp.svg',
+    logoAlt: 'C#',
+},
+{
+    logoUrl: '/assets/python.svg',
+    logoAlt: 'Python',
+},
+{
+    logoUrl: '/assets/java.svg',
+    logoAlt: 'Java',
+},
+{
+    logoUrl: '/assets/angular.svg',
+    logoAlt: 'Angular',
+}
+,{
+    logoUrl: '/assets/react.svg',
+    logoAlt: 'React',
+},
+{
+    logoUrl: '/assets/mssql.svg',
+    logoAlt: 'MSSQL',
+},
+{
+    logoUrl: '/assets/azure.svg',
+    logoAlt: 'Azure',
+}]
